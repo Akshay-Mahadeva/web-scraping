119db9de87f9bc79ddce286af4b5d77ef689b7c2
Akshay Mahadeva <akshay.mahadev94@gmail.com>
2022-07-01 18:08:45 +0530

    added libs

--- scraping.py
+++ scraping.py
@@ -1,2 +1,13 @@
-import bs4 beasutifulsoup
+from bs4 import BeautifulSoup
 import requests
+import time
+import datetime
+import smtplib
+
+# URL of the website to take data from
+URL = ''
+
+# Get headers from a specific link (Find Your User-Agent: https://httpbin.org/get)
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36"}
+
+page = requests.get(URL, headers = headers)
